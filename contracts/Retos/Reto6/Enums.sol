ceef76cc56710deb737178418a48cbbb8be4cbba
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-01-17 00:17:01 -0600

    enums

---
+++ contracts/Retos/Reto6/Enums.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract CrowdFunding {
+    enum State { Closed, Open }
+
+    struct Project{
+        string id;
+        string name;
+        string description;
+        address payable author;
+        State state;
+        uint funds;
+        uint fundraisingGoal;
+    }
+
+    Project public project;
+
+    constructor(string memory _id, string memory _name, string memory _description, uint _fundraisingGoal){
+        project = Project(_id, _name, _description, payable(msg.sender), State.Open, 0, _fundraisingGoal);
+    }
+
+    modifier sendProject() {
+        require(
+            project.author != msg.sender,
+            "author does not send money"
+        );
+        _;
+    }
+
+    event MoneyRecieveEvent(
+        address userDonator,
+        uint quantityFunds
+    );
+
+    function fundProject() public payable sendProject {
+        require(
+            project.state != State.Closed,
+            "smart contract closed"
+        );
+
+        require(
+            msg.value != 0,
+            "invalid quantity"
+        );
+
+        project.author.transfer(msg.value);
+        project.funds += msg.value;
+
+        emit MoneyRecieveEvent(msg.sender, msg.value);
+    }
+
+    event ChangeStatusEvent(
+        State newvalue
+    );
+
+    modifier stateProject() {
+        require(
+            project.author == msg.sender,
+            "Only author can change the state"
+        );
+        _;
+    }
+
+    function changeProjectState(State newState) public stateProject{
+        require(
+            newState != project.state,
+            "invalid state"
+        );
+
+        project.state = newState;
+
+        emit ChangeStatusEvent(newState);
+    }
+}
