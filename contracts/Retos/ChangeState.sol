6c45d8d45cdcc412617f981e4923b58be6348434
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-01-16 19:46:12 -0600

    change state

---
+++ contracts/Retos/ChangeState.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Change {
+    int number1 = 5;
+    
+    function ChangeState(int number) public returns (int number2) {
+        number1 = 6;
+        number2 = number + number1;
+    }
+}
