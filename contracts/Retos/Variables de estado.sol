f00030c9a6f367e1229a301cfc08000711836919
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-01-16 19:22:42 -0600

    state enviroment

---
+++ contracts/Retos/Variables de estado.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Enviroment {
+    bool isNumber;
+    int number1;
+    uint number2;
+    address owner;
+    string name;
+}
