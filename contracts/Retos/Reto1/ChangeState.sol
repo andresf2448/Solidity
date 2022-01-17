ac8ad18c95933dad383cc744ffc3482b0be66b1e
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2022-01-16 21:11:11 -0600

    sol challenge

--- contracts/Retos/Reto1/ChangeState.sol
+++ contracts/Retos/Reto1/ChangeState.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Change {
     int number1 = 5;
     
-    function ChangeState(int number) public returns (int number2) {
+    function changeState(int number) public returns (int number2) {
         number1 = 6;
         number2 = number + number1;
     }
